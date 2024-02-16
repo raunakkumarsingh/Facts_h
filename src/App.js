0bfc6475a3a7b8f8fc794dd1426a087a01a9bba7
raunakkumarsingh <raunakkumar.india@gmail.com>
2024-02-16 23:42:28 +0530

    why section added

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 import Navbar from './components/Navbar/Navbar';
-import ImageSlider from './components/ImageSlider/ImageSlider';
 import Card from './components/Card/Card'
 import Footer from './components/Footer/Footer';
 import {createBrowserRouter, RouterProvider, Route,Routes, Link, BrowserRouter as Router} from "react-router-dom";
 import Home from './Pages/Home/Home';
 import Contact from './Pages/Contacts/Contact'
+import Why from './Pages/Why/Why'
 
 const App = () => {
   return (
     <Router>
+      <div className='app-container'>
       <Navbar />
+      </div>
       <Routes>
         <Route path="/" element={ <Home/> } />
         <Route path="/Home" element={ <Home/> } />
-        <Route path="/Contact" element={ <Contact/> } />
+        <Route path="/whyfactsh" element={ <Why/> } />
+        <Route path="/contact" element={ <Contact/> } />
+        <Route path="/people" element={ <Card/> } />
        
       </Routes>
       <Footer/>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,10 +4,11 @@ import logo from '../../assets/icons/logo.png'
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="Navbar fixed-top">
+    <div className="Navbar fixed-top ">
       <span className="nav-logo"><img width="11%" height="11%" src={logo}/>Facts-H Lab</span>
       <div className={`nav-items ${isOpen && "open"}`}>
         <a href="/home">Home</a>
+        <a href="/whyfactsh">Why Facts-H?</a>
         <a href="/about">People</a>
         <a href="/service">Events</a>
         <a href="/contact">Contact</a>
