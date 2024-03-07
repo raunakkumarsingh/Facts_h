afd708da9c54e940571c89b85a6c94a7bc89b412
raunakkumarsingh <raunakkumar.india@gmail.com>
2024-03-07 22:08:55 +0530

    size-reduce

--- src/Pages/Contacts/Contact.js
+++ src/Pages/Contacts/Contact.js
@@ -1,15 +1,25 @@
 // Contact.js
 
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
 import './contact.css';
 
 
 const Contact = () => {
+  const [showMap, setShowMap] = useState(false);
+
+  useEffect(() => {
+    
+    const timer = setTimeout(() => {
+      setShowMap(true);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []); 
   return (
     <>
    
     <div className="responsivemap">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.4486909409743!2d76.64747161049772!3d9.75521459029774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b07ce23bc170053%3A0x8757971e61eb21dd!2sIndian%20Institute%20of%20Information%20Technology%20(IIIT)%20Kottayam!5e1!3m2!1sen!2sin!4v1709219395396!5m2!1sen!2sin" width="600" height="450" allowFullScreen="" Loading="eager" referrerPolicy="no-referrer-when-downgrade"></iframe>
+    <iframe className={`z-${showMap?"1":"n1"}`}  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.4486909409743!2d76.64747161049772!3d9.75521459029774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b07ce23bc170053%3A0x8757971e61eb21dd!2sIndian%20Institute%20of%20Information%20Technology%20(IIIT)%20Kottayam!5e1!3m2!1sen!2sin!4v1709219395396!5m2!1sen!2sin" width="600" height="450"  referrerPolicy="no-referrer-when-downgrade"></iframe>
     </div>
     <div className="contact-container">
       <div className="contact-card">
