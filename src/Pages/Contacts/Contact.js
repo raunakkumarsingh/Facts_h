5955005fe14c44dcf258829a5ba3e96e924164a6
raunakkumarsingh <raunakkumar.india@gmail.com>
2024-03-21 14:30:35 +0530

    bug-fix

--- src/Pages/Contacts/Contact.js
+++ src/Pages/Contacts/Contact.js
@@ -19,7 +19,8 @@ const Contact = () => {
     <>
    
     <div className="responsivemap">
-    <iframe className={`z-${showMap?"1":"n1"}`}  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.4486909409743!2d76.64747161049772!3d9.75521459029774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b07ce23bc170053%3A0x8757971e61eb21dd!2sIndian%20Institute%20of%20Information%20Technology%20(IIIT)%20Kottayam!5e1!3m2!1sen!2sin!4v1709219395396!5m2!1sen!2sin" width="600" height="450"  referrerPolicy="no-referrer-when-downgrade"></iframe>
+   
+    <iframe className={`z-${showMap?"1":"n1"}`} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3932.1281096051857!2d76.64747697486806!3d9.755214590338058!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b07ce23bc170053%3A0x8757971e61eb21dd!2sIndian%20Institute%20of%20Information%20Technology%20(IIIT)%20Kottayam!5e0!3m2!1sen!2sin!4v1711010885065!5m2!1sen!2sin" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
     <div className="contact-container">
       <div className="contact-card">
