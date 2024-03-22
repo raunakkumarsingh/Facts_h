12b1898df86d0dd08bbb9adc0132843811044eed
raunakkumarsingh <raunakkumar.india@gmail.com>
2024-03-22 14:37:38 +0530

    bug-fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,24 +7,20 @@ import iiitklogo from '../../assets/icons/IIITK_logo1.png'
 const Footer = () => {
   return (
     <footer className="footer-container">
-      <div className="footer-logos mx-2">
+    
        <img src={logo} alt="Left Logo" className="logo-left my4" />
-        <img src={iiitklogo} alt="Right Logo" className="logo-right my-1" />
-      </div>
+        
+      
       <div className="footer-address">
         <p>Indian Institute of Information Technology Kottayam,</p>
 <p>Valavoor.P.O, Pala, Kottayam,</p>
 <p>Kerala, India-686635</p>
         <p>Phone: +91-0482-2202195</p>
-      </div>
-      {/* <div className="footer-icons">
-        <a href="mailto:info@example.com"><i className="fa fa-envelope"></i></a>
-        <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin"></i></a>
-        <a href="https://www.facebook.com/yourpage" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook"></i></a>
-       
-      </div> */}
+        <hr/>
       
         <center>&copy; Copyright © 2024 All Rights Reserved.</center>
+      </div>
+      <img src={iiitklogo} alt="Right Logo" className="logo-right my-1" />
     </footer>
   );
 };
