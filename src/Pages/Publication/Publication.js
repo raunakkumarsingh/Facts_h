582107c460713309735ddc18052d3bc4e47e8e3f
raunakkumarsingh <35128126+raunakkumarsingh@users.noreply.github.com>
2024-05-28 12:25:09 +0530

    added papers

--- src/Pages/Publication/Publication.js
+++ src/Pages/Publication/Publication.js
@@ -12,31 +12,6 @@ const Publication = () => {
   return (
     <>
     <div className='pub'>
-      <Heading content="Publications"/>
-      <table className="papers-table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Authors</th>
-            <th>Conference/Journal</th>
-            <th>Publisher</th>
-            <th>Year</th>
-          </tr>
-        </thead>
-        <tbody>
-          {papers.map((paper, index) => (
-            <tr key={index} onClick={() => openWebsite(paper.website)}>
-              <td>{paper.title}</td>
-              <td>{paper.authors.join(", ")}</td>
-              <td>{paper.conference}</td>
-              <td>{paper.publisher}</td>
-              <td>{paper.year}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-    <div className='pub'>
       <Heading content="Accepted Conference"/>
       <table className="papers-table">
         <thead>
@@ -61,6 +36,32 @@ const Publication = () => {
         </tbody>
       </table>
     </div>
+    <div className='pub'>
+      <Heading content="Publications"/>
+      <table className="papers-table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Authors</th>
+            <th>Conference/Journal</th>
+            <th>Publisher</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          {papers.map((paper, index) => (
+            <tr key={index} onClick={() => openWebsite(paper.website)}>
+              <td>{paper.title}</td>
+              <td>{paper.authors.join(", ")}</td>
+              <td>{paper.conference}</td>
+              <td>{paper.publisher}</td>
+              <td>{paper.year}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+    
     </>
   );
 };
