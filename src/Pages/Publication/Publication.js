4541db76ef2c0f838e934a0854154ba6ac647cc6
raunakkumarsingh <35128126+raunakkumarsingh@users.noreply.github.com>
2024-05-28 12:31:26 +0530

    added papers

--- src/Pages/Publication/Publication.js
+++ src/Pages/Publication/Publication.js
@@ -12,31 +12,6 @@ const Publication = () => {
   return (
     <>
     <div className='pub'>
-      <Heading content="Accepted Conference"/>
-      <table className="papers-table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Authors</th>
-            <th>Conference/Journal</th>
-            {/* <th>Publisher</th> */}
-            <th>Year</th>
-          </tr>
-        </thead>
-        <tbody>
-          {acceptedPapers.map((paper, index) => (
-            <tr key={index} onClick={() => openWebsite(paper.website)}>
-              <td>{paper.title}</td>
-              <td>{paper.authors.join(", ")}</td>
-              <td>{paper.conference}</td>
-              {/* <td>{paper.publisher}</td> */}
-              <td>{paper.year}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-    <div className='pub'>
       <Heading content="Publications"/>
       <table className="papers-table">
         <thead>
@@ -61,7 +36,31 @@ const Publication = () => {
         </tbody>
       </table>
     </div>
-    
+    <div className='pub'>
+      <Heading content="Accepted Conference/Journal"/>
+      <table className="papers-table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Authors</th>
+            <th>Conference/Journal</th>
+            {/* <th>Publisher</th> */}
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          {acceptedPapers.map((paper, index) => (
+            <tr key={index} onClick={() => openWebsite(paper.website)}>
+              <td>{paper.title}</td>
+              <td>{paper.authors.join(", ")}</td>
+              <td>{paper.conference}</td>
+              {/* <td>{paper.publisher}</td> */}
+              <td>{paper.year}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
     </>
   );
 };
